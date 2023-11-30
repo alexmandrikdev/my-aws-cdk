511f99cf8fff909e958400ae9598b457ab1b619c
Alex Andras Mandrik <alexmandrik@pm.me>
2023-11-30 13:13:08 +0100

    Create an S3 bucket and a CloudFront distribution

--- lib/my-aws-cdk-stack.ts
+++ lib/my-aws-cdk-stack.ts
@@ -1,16 +1,22 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { Distribution } from 'aws-cdk-lib/aws-cloudfront';
+import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
 
-export class MyAwsCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class MyAwsCdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyAwsCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const bucket = new Bucket(this, 'MyFirstBucket', {
+        versioned: true,
+    });
+
+    // Make the bucket accessible through CloudFront distribution
+    new Distribution(this, 'MyFirstDistribution', {
+        defaultBehavior: { origin: new S3Origin(bucket) },
+    });
   }
 }
