2ee69d5102e95f0a154a9e5cb9aa4e0f5922770c
Alex Andras Mandrik <alexmandrik@pm.me>
2023-11-30 19:17:09 +0100

    Install `eslint`

--- lib/my-aws-cdk-stack.ts
+++ lib/my-aws-cdk-stack.ts
@@ -1,27 +1,27 @@
-import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import  * as s3 from 'aws-cdk-lib/aws-s3';
+import { RemovalPolicy, Stack, type StackProps } from 'aws-cdk-lib';
+import { type Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Distribution } from 'aws-cdk-lib/aws-cloudfront';
 import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
 
 export class MyAwsCdkStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor (scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
 
     const bucket = new s3.Bucket(this, 'MyFirstBucket', {
-        versioned: true,
-         removalPolicy: RemovalPolicy.DESTROY,
-        blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-        encryption: s3.BucketEncryption.S3_MANAGED,
-        enforceSSL: true,
-        autoDeleteObjects: true,
+      versioned: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      autoDeleteObjects: true
     });
 
     // Make the bucket accessible through CloudFront distribution
     new Distribution(this, 'MyFirstDistribution', {
-        defaultBehavior: { origin: new S3Origin(bucket) },
+      defaultBehavior: { origin: new S3Origin(bucket) }
     });
   }
 }
--- test/my-aws-cdk.test.ts
+++ test/my-aws-cdk.test.ts
@@ -5,13 +5,13 @@
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/my-aws-cdk-stack.ts
 test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new MyAwsCdk.MyAwsCdkStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+  //   const app = new cdk.App();
+  //     // WHEN
+  //   const stack = new MyAwsCdk.MyAwsCdkStack(app, 'MyTestStack');
+  //     // THEN
+  //   const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  //   template.hasResourceProperties('AWS::SQS::Queue', {
+  //     VisibilityTimeout: 300
+  //   });
 });
