b9007672288fc67e9b7cd48399e57ddc6d6dec08
Alex Andras Mandrik <alexmandrik@pm.me>
2023-12-04 14:32:36 +0100

    wip

--- bin/my-aws-cdk.ts
+++ bin/my-aws-cdk.ts
@@ -15,4 +15,9 @@ new MyAwsCdkStack(app, 'MyAwsCdkStack', {
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  }
 });
--- lib/my-aws-cdk-stack.ts
+++ lib/my-aws-cdk-stack.ts
@@ -1,16 +1,20 @@
 import { RemovalPolicy, Stack, type StackProps } from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib';
 import { type Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
-import { Distribution } from 'aws-cdk-lib/aws-cloudfront';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as targets from 'aws-cdk-lib/aws-route53-targets';
+
+const DOMAIN_NAME = 'images.alexmandrik.com';
 
 export class MyAwsCdkStack extends Stack {
   constructor (scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    const bucket = new s3.Bucket(this, 'MyFirstBucket', {
+    const bucket = new s3.Bucket(this, 'MyBucket', {
       versioned: true,
       removalPolicy: RemovalPolicy.DESTROY,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
@@ -19,9 +23,43 @@ export class MyAwsCdkStack extends Stack {
       autoDeleteObjects: true
     });
 
-    // Make the bucket accessible through CloudFront distribution
-    new Distribution(this, 'MyFirstDistribution', {
-      defaultBehavior: { origin: new S3Origin(bucket) }
+    new cdk.CfnOutput(this, 'BucketName', {
+      value: bucket.bucketName
+    });
+
+    const hostedZone = new route53.PublicHostedZone(this, 'HostedZone', {
+      zoneName: DOMAIN_NAME
+    });
+
+    new cdk.CfnOutput(this, 'HostedZoneId', {
+      value: hostedZone.hostedZoneId
+    });
+
+    // Output all name servers using Fn.join
+    if ((hostedZone.hostedZoneNameServers != null) && hostedZone.hostedZoneNameServers.length > 0) {
+      new cdk.CfnOutput(this, 'NameServers', {
+        value: cdk.Fn.join(', ', hostedZone.hostedZoneNameServers)
+      });
+    }
+
+    // Create a certificate in ACM for the domain
+    const certificate = new acm.Certificate(this, 'Certificate', {
+      domainName: DOMAIN_NAME,
+      validation: acm.CertificateValidation.fromDns(hostedZone)
+    });
+
+    // Create a CloudFront distribution with the certificate and the bucket as the origin
+    const distribution = new cloudfront.Distribution(this, 'MyDistribution', {
+      defaultBehavior: { origin: new S3Origin(bucket) },
+      domainNames: [DOMAIN_NAME],
+      certificate
+    });
+
+    // Create a DNS record in Route53 for the CloudFront distribution
+    new route53.ARecord(this, 'AliasRecord', {
+      zone: hostedZone,
+      recordName: DOMAIN_NAME,
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution))
     });
   }
 }
